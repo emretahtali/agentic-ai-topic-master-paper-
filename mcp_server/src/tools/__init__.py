a1231f9f629a24a27c392991216c3a959948a1c0
yigitkayabagci <yigitkayabagci@hotmail.com>
2025-12-22 18:38:00 +0300

    appointmet tools added

--- mcp_server/src/tools/__init__.py
+++ mcp_server/src/tools/__init__.py
@@ -1,2 +1,3 @@
-from .appointment_tools import AppointmentTools
+from .appointment_tools_begum import AppointmentTools
 from .tool_base import ToolBase
+from .appointment_tools import AppointmentTools
