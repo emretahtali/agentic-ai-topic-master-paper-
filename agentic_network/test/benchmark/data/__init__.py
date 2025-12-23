5d95690cd93359e41dd8909ca910783c981e9331
yigitkayabagci <yigitkayabagci@hotmail.com>
2025-12-23 16:52:58 +0300

    gemini_benchmark added

---
+++ agentic_network/test/benchmark/data/__init__.py
@@ -0,0 +1,2 @@
+from .intent_list import intent_list
+from .intent_literal import intent_literal
---
+++ agentic_network/test/benchmark/data/intent_list.py
@@ -0,0 +1,9 @@
+#TODO buraya gerçek intent list gelecek.
+intent_list = [
+    "AddAlarm", "BookAppointment", "BookHouse",
+    "BuyBusTicket", "BuyEventTickets", "BuyMovieTickets",
+    "FindAttractions", "FindBus", "FindEvents",
+    "FindHomeByArea", "FindMovies", "FindProvider",
+    "FindRestaurants", "FindTrains",
+    "GetAlarms", "GetCarsAvailable", "GetRide"
+]
